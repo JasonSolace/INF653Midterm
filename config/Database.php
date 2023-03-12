fa31e521bf9d756932b4a17ea2986fcda1220072
Jason Armenta <jasonarmenta02@gmail.com>
2023-03-12 15:25:40 -0500

    Test connection for repl

---
+++ config/Database.php
@@ -0,0 +1,36 @@
+<?php
+    class Database{
+        // Database Parameters
+        private $host;
+        private $port;
+        private $dbname;
+        private $username;
+        private $password;
+        private $conn;
+
+        public function __construct(){
+            $this->username = getenv('USERNAME');
+            $this->password = getenv('PASSWORD');
+            $this->dbname = getenv('DBNAME');
+            $this->host = getenv('HOST');
+            $this->port = getenv('PORT');
+        }
+
+        //Database Connect
+        public function connect() {
+            if ($this->conn){
+                return $this->conn;
+            } else {
+
+                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+
+                try {
+                    $this->conn = new PDO($dsn, $this->username, $this->password);
+                    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    return $this->conn;
+                }catch(PDOException $e) {
+                    echo 'Connection Error: ' . $e->getMessage();
+                }
+            }
+        }
+    }
