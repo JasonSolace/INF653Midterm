0cf946f82a01dfe3fd4fd468a74168aae231b4af
Jason Armenta <j_armenta@mail.fhsu.edu>
2023-03-12 16:17:02 -0500

    Test Bug Fix

--- config/Database.php
+++ config/Database.php
@@ -1,34 +1,35 @@
 <?php
     class Database{
-        // Database Parameters
+        private $conn;
         private $host;
         private $port;
         private $dbname;
         private $username;
         private $password;
-        private $conn;
+
 
         public function __construct(){
             $this->username = getenv('USERNAME');
-            $this->password = getenv('PASSWORD');
+            $this->password = getenv('PASSWORD'); 
             $this->dbname = getenv('DBNAME');
-            $this->host = getenv('HOST');
-            $this->port = getenv('PORT');
+            $this->host = getenv('HOST'); 
+            $this->port = getenv('PORT'); 
         }
 
-        //Database Connect
         public function connect() {
+            //instead of $this->conn = null;
             if ($this->conn){
+                //connection already exists, return it.
                 return $this->conn;
-            } else {
-
-                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+            } else{
+                
+                $dsn = 'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->dbname;
 
                 try {
                     $this->conn = new PDO($dsn, $this->username, $this->password);
                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     return $this->conn;
-                }catch(PDOException $e) {
+                } catch(PDOException $e){
                     echo 'Connection Error: ' . $e->getMessage();
                 }
             }
