afaff95715292566fb36d7e583f23fa16b18d40c
Jason Armenta <jasonarmenta02@gmail.com>
2023-03-13 13:19:09 -0500

    Add auto direct for /quotes

--- api/quotes/index.php
+++ api/quotes/index.php
@@ -1,11 +1,22 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+
 $method = $_SERVER['REQUEST_METHOD'];
+$param = isset($_GET['id']) ? $_GET['id'] : null;
+
 
 if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
+}
+
+if($method === 'GET'){
+    if(isset($param)){
+        include_once 'read_single.php';
+    } else {
+        include_once 'read.php';
+    }
 }
 ?>
