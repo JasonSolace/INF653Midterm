7f6096a16653257d28d7c077ea605acd78f4f7e1
Jason Armenta <j_armenta@mail.fhsu.edu>
2023-03-15 16:16:33 -0500

    Add Category API access with model

---
+++ api/categories/read_single.php
@@ -0,0 +1,26 @@
+    <?php
+    // Headers
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
+
+    // Instantiate DB & connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate category object
+    $category = new Category($db);
+
+    //Get ID
+    $category->id = isset($_GET['id']) ? $_GET['id'] : die();
+    
+    //Get Quote
+    $category->read_single();
+
+    $category_arr = array(
+        'id' => $category->id,
+        'category' => $category->category,
+    );
+
+    //Make JSON
+    print_r((json_encode($category_arr)));
+    
